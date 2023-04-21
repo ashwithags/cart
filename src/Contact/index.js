becb621410e7c4019096241c4b24481512bf105b
ashwithags <ashwithags@gmail.com>
2023-04-21 17:47:11 +0530

    contact functionality added

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () =>{
 
+    const [inputs, setInputs] = useState({});
+    const [textAreaInput, setTextArea]=useState('');
+
     const handleSubmit = (e)=>{
         e.preventDefault();
-        console.log(e.target.value);
+        console.log(inputs);
     }
 
     const handleChange = (e)=>{
         e.preventDefault();
+        const name = e.target.name;
+        const value = e.target.value;
+        setInputs(values => ({...values, [name]: value}))
+
+    }
+
+    const handleTextAreaChange = (e)=>{
+        e.preventDefault();
+        setTextArea(e.target.value);
+
     }
 
     return(
@@ -16,11 +29,11 @@ const Contact = () =>{
             <h3>Contact</h3>
             <div style={{textAlign:'Center'}}>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" name="firstName"  onChange={handleChange} placeholder="First Name"/><br/>
-                    <input type="text" name="lastName"  onChange={handleChange} placeholder="Last Name"/><br/>
-                    <input type="text" name="email"  onChange={handleChange} placeholder="Email"/><br/>
-                    <input type="tel" name="Phone"  onChange={handleChange} placeholder="Phone"/><br/>
-                    <textarea placeholder="Message"  onChange={handleChange}></textarea><br/>
+                    <input type="text" name="firstName" value={inputs.firstName} onChange={handleChange} placeholder="First Name"/><br/>
+                    <input type="text" name="lastName" value={inputs.lastName} onChange={handleChange} placeholder="Last Name"/><br/>
+                    <input type="text" name="email" value={inputs.email} onChange={handleChange} placeholder="Email"/><br/>
+                    <input type="tel" name="Phone" value={inputs.Phone} onChange={handleChange} placeholder="Phone"/><br/>
+                    <textarea placeholder="Message" name="message" value={inputs.message} onChange={handleTextAreaChange}></textarea><br/>
                     <button type="text">Submit</button>
                 </form>
             </div>
